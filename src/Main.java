c6b00c19d4dd0307e874241bc10791a69b9b3fa4
Semen Simanov <63959715+bpatapb88@users.noreply.github.com>
2020-05-16 11:40:36 +0200

    Add something

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World from the IntelliJ IDE!");
+        float perday = 0.5f;
+        float pathlearning = 20f;
+
+        System.out.println("Days needed:" + pathlearning/perday);
     }
 }
