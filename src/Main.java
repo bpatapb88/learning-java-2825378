f1078ca0d3bcd703b3f48590eef960a4bbebab38
Semen Simanov <63959715+bpatapb88@users.noreply.github.com>
2020-05-16 11:47:59 +0200

    Add something

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        float perday = 0.5f;
-        float pathlearning = 20f;
+        double perday = 0.5f;
+        double pathlearning = 20f;
 
         System.out.println("Days needed:" + pathlearning/perday);
     }
