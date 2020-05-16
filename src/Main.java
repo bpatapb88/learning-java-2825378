9200d0b7b83162166ba7e4592419e25aeb78f537
Semen Simanov <63959715+bpatapb88@users.noreply.github.com>
2020-05-17 00:04:20 +0200

    midnight
    midnight

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "Who is the best?";
+        String choiceOne = "you";
+        String choiceTwo = "me";
+        String choiceThree = "somebody else";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+System.out.println("Choose one of the following: " + choiceOne + "," + choiceTwo +
+        " or " + choiceThree);
         // Have the user input an answer
+        Scanner scanner = new Scanner(System.in);
         // Retrieve the user's input
-
+        String input = scanner.next();
         // If the user's input matches the correctAnswer...
+        if (correctAnswer.equals(input.toLowerCase())){
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Not correct");
+        }
         // then the user is correct and we want to print out a congrats message to the user.
 
         // If the user's input does not match the correctAnswer...
